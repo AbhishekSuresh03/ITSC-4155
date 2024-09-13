fa141b42e5747dfc283a0b8ed87f11c7ea238e3a
Hunter Starets <hunter.starets@gmail.com>
2024-09-12 22:53:19 -0400

    initial commit

---
+++ server/src/test/java/com/placeholder/trailblazer/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placeholder.trailblazer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
