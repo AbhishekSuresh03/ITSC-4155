6368eb26896301fcc7d20479ddf6e0fa6424dcd3
Cj Carrier <cjcarrier7@gmail.com>
2024-10-15 10:34:18 -0400

    setting up expo go

--- mobile/trailblazer/App.js
+++ mobile/trailblazer/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your </Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#800000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
