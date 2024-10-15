821927c25e9dd602502c21ea86e995bd5a4d5368
Hunter Starets <hunter.starets@gmail.com>
2024-10-15 11:05:38 -0400

    adding basic user model

---
+++ server/src/main/java/com/placeholder/trailblazer/model/User.java
@@ -0,0 +1,15 @@
+package com.placeholder.trailblazer.model;
+
+@Document(collection = "Users")
+
+public class User {
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+
+
+}
