ddf7dc0967b9dfa9834004c59daca6dd1823b2be
Hunter Starets <hunter.starets@gmail.com>
2024-10-16 14:02:03 -0400

    Adding UserController wireframe

---
+++ server/src/main/java/com/placeholder/trailblazer/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.placeholder.trailblazer.controller;
+
+import com.placeholder.trailblazer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
