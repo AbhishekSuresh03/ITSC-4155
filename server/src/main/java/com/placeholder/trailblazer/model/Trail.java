6e9bfea0f3bb6c1180c7e811919e9a675d94620b
Hunter Starets <hunter.starets@siemens.com>
2024-11-13 14:22:58 -0500

    adding trail model

---
+++ server/src/main/java/com/placeholder/trailblazer/model/Trail.java
@@ -0,0 +1,29 @@
+package com.placeholder.trailblazer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private ObjectId _id;
+    private String name;
+    private String city;
+    private String state;
+    private Double rating;
+    private String difficulty;
+    private Double length;
+    private int time;
+    private int pace;
+    private String profilePic;
+    private String userName;
+    private Date date;
+    private String description;
+}
