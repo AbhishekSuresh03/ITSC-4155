7b206cf75c463b5deaa9c92b67b72db17db299d6
Hunter Starets <hunter.starets@gmail.com>
2024-11-19 22:20:46 -0500

    updating user and trail models and upating their respective docs

--- server/src/main/java/com/placeholder/trailblazer/model/Trail.java
+++ server/src/main/java/com/placeholder/trailblazer/model/Trail.java
@@ -7,13 +7,13 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.List;
 
 @Document(collection = "Trails")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Trail {
-
     @Id
     private String id;
     private String name;
@@ -22,9 +22,9 @@ public class Trail {
     private Double rating;
     private String difficulty;
     private Double length;
-    private int time;
-    private int pace;
-    private String profilePic;
+    private Double time;
+    private Double pace;
+    private List<String> images;
     private String username;
     private Date date;
     private String description;
