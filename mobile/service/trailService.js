2e4c6138fbe5a782dc10cc518e28b35417a3fbb3
Hunter Starets <hunter.starets@gmail.com>
2024-11-20 22:55:40 -0500

    Expanding trails to accommodate owners.

--- mobile/service/trailService.js
+++ mobile/service/trailService.js
@@ -16,3 +16,38 @@ export async function fetchTrails() {
         throw error;
     }
 }
+
+export async function fetchTrailById(id) {
+    try {
+        const response = await fetch(`${BASE_URL}/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function createTrail(formData, userId) {
+    try {
+        const response = await fetch(`${BASE_URL}?ownerId=${userId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+}
